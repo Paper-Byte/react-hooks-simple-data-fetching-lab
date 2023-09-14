83786479208074abf19a9af8e7eb084a7a3f0bb7
Noah <noahhughesph@gmail.com>
2023-09-14 18:50:11 -0400

    all tests passing

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,26 @@
 // create your App component here
+import React, { useState, useEffect } from 'react';
+
+const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [dogImage, setDogImage] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        'https://dog.ceo/api/breeds/image/random'
+      );
+      const data = await response.json();
+      setDogImage(data);
+      setIsLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  return <img src={dogImage.message} alt="A Random Dog"></img>;
+};
+
+export default App;
